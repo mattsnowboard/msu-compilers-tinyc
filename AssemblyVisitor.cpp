a45707536495c077043ffa661b2837d3e22c9745
Matt Durak <durakmat@msu.edu>
2012-04-02 18:08:47 -0400

    Adding a skeleton AssemblyVisitor for where we do the actual work of making assembly code

---
+++ AssemblyVisitor.cpp
@@ -0,0 +1,66 @@
+#include "AssemblyVisitor.h"
+
+
+void AssemblyVisitor::Visit(const Program & p)
+{
+    // Visit each FunctionBlock in Program
+}
+
+void AssemblyVisitor::Visit(const FunctionBlock & f)
+{
+    // get the current SymbolTable and store it on a stack of symbol tables?
+    // Print prolog
+    // ? then visit all statements
+    // Print epilog
+}
+
+void AssemblyVisitor::Visit(const Add & a)
+{
+    // use a binary helper for visit left, visit right, pop, pop, add, push
+}
+
+void AssemblyVisitor::Visit(const Divide & d)
+{
+}
+
+void AssemblyVisitor::Visit(const Multiply & m)
+{
+}
+
+void AssemblyVisitor::Visit(const Negate & n)
+{
+}
+
+void AssemblyVisitor::Visit(const Subtract & s)
+{
+}
+
+void AssemblyVisitor::Visit(const Value & v)
+{
+    // push on stack?
+}
+
+void AssemblyVisitor::Visit(const Variable & v)
+{
+    // Look up in current symbol table, push some offset of ebp on stack
+}
+
+void AssemblyVisitor::Visit(const AssignStmt & a)
+{
+    // pop something and store it in some offset to ebp depending on var
+}
+
+void AssemblyVisitor::Visit(const WriteStmt & w)
+{
+    // may need to evaluate the RHS, then just output "write ???"
+}
+
+void AssemblyVisitor::Visit(const DeclStmt & d)
+{
+    // allocate space on stack for local, put in current symbol table?
+}
+
+void AssemblyVisitor::Visit(const ReturnStmt & r)
+{
+    // return statement means writing something to eax
+}
---
+++ AssemblyVisitor.h
@@ -0,0 +1,52 @@
+#ifndef _ASSEMBLYVISITOR_H
+#define _ASSEMBLYVISITOR_H
+
+#include "StatementVisitor.h"
+
+#include <iostream>
+
+class Program;
+class FunctionBlock;
+
+class Add;
+class Divide;
+class Multiply;
+class Negate;
+class Subtract;
+class Value;
+class Variable;
+class FuncCall;
+
+class WriteStmt;
+class AssignStmt;
+class DeclStmt;
+class ReturnStmt;
+
+class AssemblyVisitor : public StatementVisitor
+{
+public:
+    AssemblyVisitor(std::ostream &out) :
+        _out(out)
+    {}
+
+    virtual void Visit(const Program &p);
+    virtual void Visit(const FunctionBlock &f);
+
+    virtual void Visit(const Add &a);
+    virtual void Visit(const Divide &d);
+    virtual void Visit(const Multiply &m);
+    virtual void Visit(const Negate &n);
+    virtual void Visit(const Subtract &s);
+    virtual void Visit(const Value &v);
+    virtual void Visit(const Variable &v);
+
+    virtual void Visit(const AssignStmt &a);
+    virtual void Visit(const WriteStmt &w);
+    virtual void Visit(const DeclStmt &d);
+    virtual void Visit(const ReturnStmt &r);
+
+private:
+    std::ostream &_out;
+};
+
+#endif
